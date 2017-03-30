09c3c8f43b794845c0e139621e4611967f5334d4
Andrew Prosikhin <andrewpros@mail.ru>
2017-03-29 21:22:12 -0400

    System is working. Nginx is acting as a load balancer.

--- launch.sh
+++ launch.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-
+docker network create simple-network
 docker build -t server_1_image -f server_1.df .
 docker build -t server_2_image -f server_2.df .
-docker run -d -p 8000:8000 server_1_image
-docker run -d -p 8001:8000 server_2_image
-curl localhost:8000
-curl localhost:8001
+docker run -d --net=simple-network --name SOne server_1_image
+docker run -d --net=simple-network --name STwo server_2_image
+docker build -t docker_lb/basic-proxy -f basic_proxy.df .
+docker run -d -p 80:80 --net=simple-network docker_lb/basic-proxy
+curl localhost:80
+curl localhost:80
+curl localhost:80
+curl localhost:80
+curl localhost:80
+curl localhost:80
+curl localhost:80
+curl localhost:80
+curl localhost:80
+curl localhost:80
